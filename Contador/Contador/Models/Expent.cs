a87fa3af2acaa67afc228349360dd7cbdddac7f3
Lucas Xavier <lukasx1934@gmail.com>
2023-09-11 19:39:14 -0300

    Adicionando construtor da classe Expent

--- Contador/Contador/Models/Expent.cs
+++ Contador/Contador/Models/Expent.cs
@@ -5,7 +5,14 @@
         public string Descricao { get; set; }
         public decimal Value { get; set; }
         public DateTime Date { get; set; }
-        public Category Category { get; set; }
+        public Category? Category { get; set; }
 
+        public Expent(string descricao, decimal value, DateTime date, Category? category)
+        {
+            Descricao = descricao;
+            Value = value;
+            Date = date;
+            Category = category;
+        }
     }
 }
