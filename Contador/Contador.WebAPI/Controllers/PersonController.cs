998c194c4318eda8e5b2fe93c65d299e8a3da290
Lucas Xavier <lukasx1934@gmail.com>
2023-09-11 19:51:56 -0300

    Adicionando controller de exemplo PersonController.

---
+++ Contador/Contador.WebAPI/Controllers/PersonController.cs
@@ -0,0 +1,22 @@
+﻿using Contador.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Contador.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetPerson()
+        {
+            Person person = new Person("Lucas Xavier", "06666000600");
+
+            return Ok(new
+            {
+                Data = person
+            });
+        }
+    }
+}
