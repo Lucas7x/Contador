4514cabb531b5812cc59b6778290ff93aec01cce
Lucas Xavier <lukasx1934@gmail.com>
2023-09-11 22:28:39 -0300

    Iniciando configuração do DbContext

---
+++ Contador/Contador/Context/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Contador.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contador.Context
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer(
+                "Data Source=Localhost;" +
+                "Initial Catalog=ContadorDB;" +
+                "Integrated Security=True"
+                );
+    }
+}
--- Contador/Contador/Models/Person.cs
+++ Contador/Contador/Models/Person.cs
@@ -2,8 +2,8 @@
 {
     public class Person : BaseModel
     {
-        public string Name { get; set; }
-        public string? Cpf { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Cpf { get; set; } = string.Empty;
 
         public Person(string name, string? cpf)
         {
