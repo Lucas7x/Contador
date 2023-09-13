e3a23778f845324cd41224171dcbe3ca4ccf9522
Lucas Xavier <lukasx1934@gmail.com>
2023-09-13 00:15:08 -0300

    Adicionando métodos Get e Post na PersonController.

--- Contador/Contador.WebAPI/Controllers/PersonController.cs
+++ Contador/Contador.WebAPI/Controllers/PersonController.cs
@@ -1,22 +1,59 @@
-﻿using Contador.Models;
-using Microsoft.AspNetCore.Http;
+﻿using Contador.Context;
+using Contador.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Contador.WebAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class PersonController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult GetPerson()
+        [HttpGet("Id")]
+        public async Task<IActionResult> GetPersonById(long id)
         {
-            Person person = new Person("Lucas Xavier", "06666000600");
+            try
+            {
+                using var context = new DataContext();
+                Person person = await context.Persons.FirstOrDefaultAsync(x => x.Id == id);
+                return Ok(person);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
 
+        [HttpGet]
+        public async Task<IActionResult> GetPersons()
+        {
+            using var context = new DataContext();
+            List<Person> persons = await context.Persons.ToListAsync();
             return Ok(new
             {
-                Data = person
+                Persons = persons
             });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostPersonAsync(Person person)
+        {
+            try
+            {
+                using var context = new DataContext();
+                await context.Persons.AddAsync(person);
+                await context.SaveChangesAsync();
+
+                return Ok(new
+                {
+                    Success = true,
+                    Message = "Pessoa adicionada com sucesso."
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
