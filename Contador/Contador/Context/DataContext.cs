2b831b91abbc0cebdfd27c38e13e023b150a9608
Lucas Xavier <36903182+Lucas7x@users.noreply.github.com>
2023-09-26 20:22:28 -0300

    Adicionando Category no Context

--- Contador/Contador/Context/DataContext.cs
+++ Contador/Contador/Context/DataContext.cs
@@ -8,6 +8,8 @@ namespace Contador.Context
     {
         public DbSet<Person> Persons { get; set; } = null;
 
+        public DbSet<Category> Categories { get; set; } = null;
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<Person>()
@@ -19,6 +21,16 @@ namespace Contador.Context
                 .Property(p => p.Cpf)
                 .HasMaxLength(14)
                 .IsRequired(false);
+
+            builder.Entity<Category>()
+                .Property (p => p.Name)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Entity<Category>()
+                .Property (p => p.Description)
+                .HasMaxLength(500)
+                .IsRequired();
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
