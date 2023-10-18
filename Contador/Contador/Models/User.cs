093ee1432aae7ee8ab0f7c1047551ea601e598b1
Lucas Xavier <36903182+Lucas7x@users.noreply.github.com>
2023-10-17 22:39:01 -0300

    Adicionando classes User e Wallet

---
+++ Contador/Contador/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Contador.Models;
+
+public class User : BaseModel
+{
+    public Person Person { get; set; }
+    public string Login { get; set; }
+    public string Password { get; set; }
+
+    public User(Person person, string login, string password)
+    {
+        Person = person;
+        Login = login;
+        Password = password;
+    }
+}
---
+++ Contador/Contador/Models/Wallet.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Contador.Models
+{
+    public class Wallet
+    {
+        public string Name {  get; set; }
+        public float Balance { get; set; }
+        public Person person { get; set; }
+        
+        public void Deposito (float value )
+        {
+            Balance = Balance + value;
+            
+        }
+        
+        public bool Saque(float value) {
+            if (value > Balance) return false;
+            else {
+                
+                Balance = Balance - value;
+                return true; 
+            }
+        }
+    }
+}
