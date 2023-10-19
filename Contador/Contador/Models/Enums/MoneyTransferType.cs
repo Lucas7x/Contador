b0776376199a3fdbeb6fff25945a7c4211568831
Lucas Xavier <36903182+Lucas7x@users.noreply.github.com>
2023-10-18 22:14:46 -0300

    Adicionando classe MoneyTransfer

---
+++ Contador/Contador/Models/Enums/MoneyTransferType.cs
@@ -0,0 +1,7 @@
+﻿namespace Contador.Models.Enums;
+
+public enum MoneyTransferType
+{
+    TransferIn = 0,
+    TransferOut = 2
+}
---
+++ Contador/Contador/Models/MoneyTransfer.cs
@@ -0,0 +1,12 @@
+﻿using Contador.Models.Enums;
+
+namespace Contador.Models;
+
+public class MoneyTransfer : BaseModel
+{
+    public double Amount { get; set; }
+    public MoneyTransferType Type { get; set; }
+    public string Description { get; set; }
+    public Wallet? OriginWallet { get; set; }
+    public Wallet? DestinationWallet { get; set; }
+}
