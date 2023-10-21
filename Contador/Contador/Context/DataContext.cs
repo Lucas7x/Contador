c515477b6e9098e411d4c5d39bef28208445d4b8
Lucas Xavier <36903182+Lucas7x@users.noreply.github.com>
2023-10-21 18:19:35 -0300

    Migration das classes

--- Contador/Contador/Context/DataContext.cs
+++ Contador/Contador/Context/DataContext.cs
@@ -1,43 +1,46 @@
 ﻿using Contador.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace Contador.Context
+namespace Contador.Context;
+
+public class DataContext : DbContext
 {
-    public class DataContext : DbContext
+    public DbSet<User> Users { get; set; } = null;
+    public DbSet<Person> Persons { get; set; } = null;
+    public DbSet<Category> Categories { get; set; } = null;
+    public DbSet<Wallet> Wallets { get; set; } = null;
+    public DbSet<Debt> Debts { get; set; } = null;
+    public DbSet<Installment> Installments { get; set; } = null;
+    public DbSet<MoneyTransfer> MoneyTransfer { get; set; } = null;
+
+    protected override void OnModelCreating(ModelBuilder builder)
     {
-        public DbSet<Person> Persons { get; set; } = null;
+        builder.Entity<Person>()
+            .Property(p => p.Name)
+            .HasMaxLength(100)
+            .IsRequired();
 
-        public DbSet<Category> Categories { get; set; } = null;
+        builder.Entity<Person>()
+            .Property(p => p.Cpf)
+            .HasMaxLength(14)
+            .IsRequired(false);
 
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            builder.Entity<Person>()
-                .Property(p => p.Name)
-                .HasMaxLength(100)
-                .IsRequired();
+        builder.Entity<Category>()
+            .Property(p => p.Name)
+            .HasMaxLength(100)
+            .IsRequired();
 
-            builder.Entity<Person>()
-                .Property(p => p.Cpf)
-                .HasMaxLength(14)
-                .IsRequired(false);
-
-            builder.Entity<Category>()
-                .Property(p => p.Name)
-                .HasMaxLength(100)
-                .IsRequired();
-
-            builder.Entity<Category>()
-                .Property(p => p.Description)
-                .HasMaxLength(500)
-                .IsRequired();
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlServer(
-                "Data Source=DESKTOP-LTBAOUJ\\SQLEXPRESS;" +
-                "Initial Catalog=ContadorDB;" +
-                "Integrated Security=True;" +
-                "TrustServerCertificate=True"
-                );
+        builder.Entity<Category>()
+            .Property(p => p.Description)
+            .HasMaxLength(500)
+            .IsRequired();
     }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer(
+            "Data Source=DESKTOP-LTBAOUJ\\SQLEXPRESS;" +
+            "Initial Catalog=ContadorDB;" +
+            "Integrated Security=True;" +
+            "TrustServerCertificate=True"
+            );
 }
---
+++ Contador/Contador/Migrations/20231021211811_AddClassesMigration.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Contador.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddClassesMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletionDate",
+                table: "Persons",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Mail",
+                table: "Persons",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Persons",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletionDate",
+                table: "Categories",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateTable(
+                name: "Debts",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoryId = table.Column<long>(type: "bigint", nullable: false),
+                    Value = table.Column<double>(type: "float", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DeletionDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Debts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Debts_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PersonId = table.Column<long>(type: "bigint", nullable: false),
+                    Login = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DeletionDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Persons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Wallets",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Balance = table.Column<float>(type: "real", nullable: false),
+                    personId = table.Column<long>(type: "bigint", nullable: false),
+                    DeletionDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Wallets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Wallets_Persons_personId",
+                        column: x => x.personId,
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Installments",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DebtId = table.Column<long>(type: "bigint", nullable: false),
+                    Value = table.Column<double>(type: "float", nullable: false),
+                    DueDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    DeletionDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Installments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Installments_Debts_DebtId",
+                        column: x => x.DebtId,
+                        principalTable: "Debts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MoneyTransfer",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DebtId = table.Column<long>(type: "bigint", nullable: false),
+                    Amount = table.Column<double>(type: "float", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OriginWalletId = table.Column<long>(type: "bigint", nullable: true),
+                    DestinationWalletId = table.Column<long>(type: "bigint", nullable: true),
+                    DeletionDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MoneyTransfer", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MoneyTransfer_Debts_DebtId",
+                        column: x => x.DebtId,
+                        principalTable: "Debts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MoneyTransfer_Wallets_DestinationWalletId",
+                        column: x => x.DestinationWalletId,
+                        principalTable: "Wallets",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_MoneyTransfer_Wallets_OriginWalletId",
+                        column: x => x.OriginWalletId,
+                        principalTable: "Wallets",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Debts_CategoryId",
+                table: "Debts",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Installments_DebtId",
+                table: "Installments",
+                column: "DebtId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MoneyTransfer_DebtId",
+                table: "MoneyTransfer",
+                column: "DebtId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MoneyTransfer_DestinationWalletId",
+                table: "MoneyTransfer",
+                column: "DestinationWalletId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MoneyTransfer_OriginWalletId",
+                table: "MoneyTransfer",
+                column: "OriginWalletId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_PersonId",
+                table: "Users",
+                column: "PersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Wallets_personId",
+                table: "Wallets",
+                column: "personId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Installments");
+
+            migrationBuilder.DropTable(
+                name: "MoneyTransfer");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Debts");
+
+            migrationBuilder.DropTable(
+                name: "Wallets");
+
+            migrationBuilder.DropColumn(
+                name: "DeletionDate",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "Mail",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "DeletionDate",
+                table: "Categories");
+        }
+    }
+}
