bc8b288cad40522b43dcf53f5611803a590ef1ae
Lucas Xavier <36903182+Lucas7x@users.noreply.github.com>
2023-10-21 18:15:20 -0300

    Removendo construtor da classe User

--- Contador/Contador/Models/User.cs
+++ Contador/Contador/Models/User.cs
@@ -5,11 +5,4 @@ public class User : BaseModel
     public Person Person { get; set; }
     public string Login { get; set; }
     public string Password { get; set; }
-
-    public User(Person person, string login, string password)
-    {
-        Person = person;
-        Login = login;
-        Password = password;
-    }
 }
