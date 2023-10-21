72ba94ad2d979722a7caeb0c07dd1e30ed601026
Lucas Xavier <36903182+Lucas7x@users.noreply.github.com>
2023-10-21 11:52:28 -0300

    Adicionando classes Debt e Installment

---
+++ Contador/Contador/Models/Debt.cs
@@ -0,0 +1,8 @@
+﻿namespace Contador.Models;
+
+public class Debt : BaseModel
+{
+    public string Description { get; set; }
+    public double Value { get; set; }
+    public DateTime Date { get; set; }
+}
---
+++ Contador/Contador/Models/Installment.cs
@@ -0,0 +1,8 @@
+﻿namespace Contador.Models;
+
+public class Installment : BaseModel
+{
+    public Debt Debt { get; set; }
+    public double Value { get; set; }
+    public DateTime DueDate { get; set; }
+}
