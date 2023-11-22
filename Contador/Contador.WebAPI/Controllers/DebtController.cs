a9f48b3fc4800e818283caa3448ac3744c5d14b5
Lucas Xavier <36903182+Lucas7x@users.noreply.github.com>
2023-11-21 22:32:32 -0300

    Criando DebtController

---
+++ Contador/Contador.WebAPI/Controllers/DebtController.cs
@@ -0,0 +1,83 @@
+﻿using Contador.Context;
+using Contador.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contador.WebAPI.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class DebtController : ControllerBase
+{
+    [HttpGet("Id")]
+    public async Task<IActionResult> GetDebtById(long id)
+    {
+        try
+        {
+            using var context = new DataContext();
+            Debt debt = await context.Debts.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(new
+            {
+                Success = true,
+                Debt = debt
+            });
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new
+            {
+                Success = false,
+                Message = ex.Message
+            });
+        }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetDebts()
+    {
+        try
+        {
+            using var context = new DataContext();
+            List<Debt> debts = await context.Debts.ToListAsync();
+            return Ok(new
+            {
+                Success = true,
+                Debts = debts
+            });
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new
+            {
+                Success = false,
+                Message = ex.Message
+            });
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostDebt(Debt debt)
+    {
+        try
+        {
+            using var context = new DataContext();
+            await context.Debts.AddAsync(debt);
+            await context.SaveChangesAsync();
+
+            return Ok(new
+            {
+                Success = true,
+                Debt = debt,
+                Message = "Dívida adicionada com sucesso."
+            });
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new
+            {
+                Success = false,
+                Message = ex.Message
+            });
+        }
+    }
+}
